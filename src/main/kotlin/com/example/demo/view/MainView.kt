6c2eea65b561e5a2163dad0be592e65f044ad0fc
jlillywh <jason.lillywhite@gmail.com>
2019-11-25 07:31:33 -0800

    added layout components to main view

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,14 +1,47 @@
 package com.example.demo.view
 
 import com.example.demo.app.Styles
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
 import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
-    override val root = hbox {
+    val topView: TopView by inject()
+    val leftView: LeftView by inject()
+    val centerView: CenterView by inject()
+    val rightView: RightView by inject()
+    val bottomView: BottomView by inject()
+
+    override val root = borderpane() {
+        top = topView.root
+        left = leftView.root
+        center = centerView.root
+        right = rightView.root
+        bottom = bottomView.root
+    }
+}
+
+class TopView: View() {
+    override val root = label("Top View") {
         label("Reservoir Model") {
             addClass(Styles.heading)
-        button("Build it!")
         }
     }
+}
+
+class LeftView: View() {
+    override  val root  = label("Left")
+}
+
+class CenterView: View() {
+    override  val root  = label("Center")
+}
+
+class RightView: View() {
+    override  val root  = label("Right")
+}
+
+class BottomView: View() {
+    override val root = label("Bottom View")
 }
 
